dbd72d78c2fe235bed6e4a2cb8147fd9d9b98a76
Ziyanlu16 <caixukunwuebuzuozhu@gmail.com>
2023-11-15 22:03:17 +0800

    main

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from flaskblog import db, app
-from flaskblog.models import User, Post
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
 
+s = Serializer('your-secret-key')  # 使用安全的秘钥
 
-post = Post()
+# 生成加密令牌并解码，设置过期时间为 30 分钟
+token = s.dumps({'user_id': 1}, expires_in=1800).decode('utf-8')
+
+# 打印令牌
+print(token)
