d53ac35b56d9003ec80952357f8c873898071adf
Ziyanlu16 <caixukunwuebuzuozhu@gmail.com>
2023-11-16 22:41:10 +0800

    main

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -58,6 +58,7 @@ class UpdateAccountForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    
     submit = SubmitField('Post')
 
 
